113c285154692e7dc368a98fee6524674e9ef3e6
foo <foo@bar>
2021-01-22 20:11:43 +0900

    up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   get 'cards/index'
-  get 'cards/show'
+  get 'cards', to: 'cards#index'
+
   get 'cards/add'
-  get 'cards/edit'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'cards/add'
+
+  get 'cards/:id', to: 'cards#show'
+
+  get 'cards/edit/:id', to: 'cards#edit'
+  post 'cards/edit/:id', to: 'cards#edit'
+
+  get 'cards/delete/:id', to: 'cards#delete'
 end
